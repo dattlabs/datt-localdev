f4755a313a32ecb549f88a285aaee2a8b86b084c
John Albietz <inthecloud247@gmail.com>
2014-03-14 01:56:26 -0700

    added examples

---
+++ tests/virtualbox_is_installed.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bats
+
+@test "virtualbox: invoking vboxmanage prints a 4.x version string" {
+  result="$(vboxmanage -version)"
+ [[ $result =~ ^4\.* ]]
+}
+
+@test "virtualbox: 4 vbox kernel modules loaded" {
+# Example:
+# vboxpci                23194  0
+# vboxnetadp             25670  0
+# vboxnetflt             27613  0
+# vboxdrv               335365  4 vboxnetadp,vboxnetflt,vboxpci
+
+  result="$(lsmod | grep vbox | wc -l)"
+  [[ $result = 4 ]]
+}
+
